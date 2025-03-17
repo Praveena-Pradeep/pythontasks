0cc81fe4d472fb68d7290be92819e0730e9f64f1
Praveena Pradeep <168461511+Praveena-Pradeep@users.noreply.github.com>
2025-03-17 15:54:02 +0530

    Save local changes to fix_start.py

--- fix_start.py
+++ fix_start.py
@@ -1,9 +1,8 @@
 def fix_start(s):
     first_char = s[0]
-    print(f"First character: {first_char}")  # Debugging line
     return first_char + s[1:].replace(first_char, '*')
 
 word = input("Enter the string: ")
 
 result = fix_start(word)
-print(f"Result: {result}")  # Debugging line
+print(f"Result: {result}") 
